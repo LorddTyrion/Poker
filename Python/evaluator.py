7d070ceab8f6e0897af1474f1fd819ff9e9eecec
LorddTyrion <baczodomonkos@gmail.com>
2025-05-01 21:58:23 +0200

    preflop evaluation

--- Python/evaluator.py
+++ Python/evaluator.py
@@ -60,14 +60,38 @@ class HandEvaluator:
             else:
                 losses +=1
         return [wins/num_simulations, draws/num_simulations, losses/num_simulations]
+    def simulate_preflop_games(self, num_simulations):
+        evaluator = Evaluator()
+        wins = 0
+        draws = 0
+        losses = 0
+        remaining_cards = np.concatenate([self.remaining_cards, self.board], axis=0)
+        for i in range(num_simulations):
+            simulated_hand = np.random.choice(remaining_cards, 2, replace=False)
+            cards_for_board = [card for card in remaining_cards if card not in simulated_hand]
+            simulated_board = np.random.choice(cards_for_board, 5, replace=False)
+            eval = evaluator.evaluate(simulated_board.tolist(), self.hand)
+            simulated_eval = evaluator.evaluate(simulated_board.tolist(), simulated_hand.tolist())
+            if eval < simulated_eval:
+                wins +=1
+            elif eval == simulated_eval:
+                draws +=1
+            else:
+                losses +=1
+        return [wins/num_simulations, draws/num_simulations, losses/num_simulations]
     
 class HandClustering:
 
     def __init__(self):
+        self.preflop_map = {}
         self.flop_map = {}
         self.turn_map = {}
         self.river_map = {}
 
+    def get_preflop_cluster(self, hand):
+        if tuple(hand) in self.preflop_map:
+            return self.preflop_map[tuple(hand)]
+        
     def get_flop_cluster(self, eval):
         if eval in self.flop_map:
             return self.flop_map[eval]
@@ -79,6 +103,21 @@ class HandClustering:
     def get_river_cluster(self, eval):
         if eval in self.river_map:
             return self.river_map[eval]
+    
+    def build_preflop_table(self, num_simulations):
+        full_deck1 = Deck().GetFullDeck()
+        full_deck2 = Deck().GetFullDeck()
+        pairs = [(card1, card2) for card1 in full_deck1 for card2 in full_deck2 if card1 != card2]
+        results = []
+        for pair in pairs:
+            remaining_cards = [card for card in full_deck1 if card not in pair]
+            board = np.random.choice(remaining_cards, 5, replace=False)
+            he = HandEvaluator(list(pair), board)
+            results.append(he.simulate_preflop_games(num_simulations))
+        clusters = self.get_clusters(10, results)
+        for i, pair in enumerate(pairs):
+            self.preflop_map[pair] = clusters[i]
+
         
     def build_flop_table(self, num_simulations):
         deck = Deck()
@@ -123,4 +162,8 @@ class HandClustering:
 
 hc = HandClustering()
 hc.build_river_table(100)
-hc.get_river_cluster(4500)
+print(hc.get_river_cluster(4500))
+hc.build_preflop_table(100)
+deck = Deck()
+hand = deck.draw(2)
+print(hc.get_preflop_cluster(hand))
